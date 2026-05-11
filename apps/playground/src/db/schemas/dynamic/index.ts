01f43757c74caf378447ad99898d47b96a91a065
Mobeen Abdullah <mobeenabdullah@gmail.com>
2026-05-11 13:14:00 +0500

    Clean up exports in dynamic index file
    Removed multiple exports from dynamic index file.

--- apps/playground/src/db/schemas/dynamic/index.ts
+++ apps/playground/src/db/schemas/dynamic/index.ts
@@ -3,17 +3,4 @@
 // Example: export { dc_products } from './products';
 
 // Empty export to make this a valid TypeScript module
-export {};export { dc_job } from './job';
-export { dc_event } from './event';
-export { dc_people } from './people';
-export { dc_photo } from './photo';
-export { dc_legend } from './legend';
-export { dc_zerofieldtest } from './zerofieldtest';
-export { dc_statustest } from './statustest';
-export { dc_caseee } from './caseee';
-export { dc_game } from './game';
-export { dc_webinar } from './webinar';
-export { dc_article } from './article';
-export { dc_dev } from './dev';
-export { dc_bill } from './bill';
-export { dc_student } from './student';
+export {};
