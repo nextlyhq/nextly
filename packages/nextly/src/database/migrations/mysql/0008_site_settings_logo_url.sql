e911b77840797680806b50bbb3ff8b3825702726
aqib-rx <aqib.revnix@gmail.com>
2026-06-01 14:42:14 +0500

    refactor(nextly): remove bundled core SQL + drizzle.config (§4.5)

--- packages/nextly/src/database/migrations/mysql/0008_site_settings_logo_url.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add logo_url column to site_settings
-
--- UP
-
-ALTER TABLE `site_settings` ADD COLUMN `logo_url` varchar(2048);
-
--- DOWN
-
-ALTER TABLE `site_settings` DROP COLUMN `logo_url`;
